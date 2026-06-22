bc2474c48d61b69b184fdd16c6eac60a102c77f3
Rhia Singh <rhia.singh22@gmail.com>
2026-06-22 15:41:53 -0400

    Prevent empty todos

--- to_do_events.js
+++ to_do_events.js
@@ -6,17 +6,17 @@ MyButton.addEventListener("click", function() {
     const inputElement = document.getElementById("user_input")
     const currentText = inputElement.value;
 
-    // 1. Find the existing list container
-    const listContainer = document.getElementById('My_List');
+    if (currentText.trim() !== ""){
+        // 1. Find the existing list container
+        const listContainer = document.getElementById('My_List');
 
-    // 2. Create the new <li> element
-    const newListItem = document.createElement('li');
+        // 2. Create the new <li> element
+        const newListItem = document.createElement('li');
 
-    // 3. Add text content to the new item
-    newListItem.textContent = currentText;
+        // 3. Add text content to the new item
+        newListItem.textContent = currentText;
 
-    // 4. Append the new item to the list container
-    listContainer.appendChild(newListItem);
-
-
+        // 4. Append the new item to the list container
+        listContainer.appendChild(newListItem);
+    }
 });
