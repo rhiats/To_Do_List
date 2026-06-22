121812ebbc6a0a7430ecbbdec76d24b46498f2ed
Rhia Singh <rhia.singh22@gmail.com>
2026-06-22 15:28:11 -0400

    Remove temporary list elements

--- to_do_events.js
+++ to_do_events.js
@@ -6,4 +6,9 @@ MyButton.addEventListener("click", function() {
     //console.log("The button was clicked!");
     //const outputElement = document.getElementById("status");
     //outputElement.textContent = "The button was clicked!";
+
+    const inputElement = document.getElementById("user_input")
+    const currentText = inputElement.value;
+
+    
 });
