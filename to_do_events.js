ab3b9695a81595c7acc33c1485b6e1f458722030
Rhia Singh <rhia.singh22@gmail.com>
2026-06-22 15:31:22 -0400

    add list elements using javascript

--- to_do_events.js
+++ to_do_events.js
@@ -3,12 +3,20 @@ const MyButton = document.getElementById("input_button")
 
 // 2. Attach a listener
 MyButton.addEventListener("click", function() {
-    //console.log("The button was clicked!");
-    //const outputElement = document.getElementById("status");
-    //outputElement.textContent = "The button was clicked!";
-
     const inputElement = document.getElementById("user_input")
     const currentText = inputElement.value;
 
-    
+    // 1. Find the existing list container
+    const listContainer = document.getElementById('My_List');
+
+    // 2. Create the new <li> element
+    const newListItem = document.createElement('li');
+
+    // 3. Add text content to the new item
+    newListItem.textContent = currentText;
+
+    // 4. Append the new item to the list container
+    listContainer.appendChild(newListItem);
+
+
 });
