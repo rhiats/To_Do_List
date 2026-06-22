112ffacf4462f5462865b791f988f2a8703b6e1c
Rhia Singh <rhia.singh22@gmail.com>
2026-06-22 15:20:45 -0400

    check event listener and debug

--- to_do_events.js
+++ to_do_events.js
@@ -1,7 +1,9 @@
 //select button
-const MyButton = document.getElementById("button")
+const MyButton = document.getElementById("input_button")
 
 // 2. Attach a listener
 MyButton.addEventListener("click", function() {
-    console.log("The button was clicked!");
+    //console.log("The button was clicked!");
+    //const outputElement = document.getElementById("status");
+    //outputElement.textContent = "The button was clicked!";
 });
