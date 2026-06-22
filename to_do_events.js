dc914571c4fa05168103e6a213d28026fa1668a1
Rhia Singh <rhia.singh22@gmail.com>
2026-06-22 15:04:02 -0400

    Attach a listener

--- to_do_events.js
+++ to_do_events.js
@@ -1,1 +1,7 @@
-const button = document.getElementById("button")
+//select button
+const MyButton = document.getElementById("button")
+
+// 2. Attach a listener
+MyButton.addEventListener("click", function() {
+    console.log("The button was clicked!");
+});
