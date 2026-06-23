05ed4450df32504423bb2157da16ba42f1b03bb5
Rhia Singh <rhia.singh22@gmail.com>
2026-06-23 15:06:20 -0400

    download dataset button

--- to_do_events.js
+++ to_do_events.js
@@ -58,3 +58,22 @@ MyButton.addEventListener("click", function() {
     }
 });
 
+// Download dataset button
+const downloadBtn = document.getElementById("download_btn");
+
+downloadBtn.addEventListener("click", function () {
+    const dataStr = JSON.stringify(data_arr, null, 2);
+
+    const blob = new Blob([dataStr], { type: "application/json" });
+
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "todo_dataset.json";
+
+    a.click();
+
+    URL.revokeObjectURL(url);
+});
+
